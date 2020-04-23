3ab421935028d757f78f6fa1d9ae4c39dad53ee3
Lucas Cunha <lucasdinizcunha@uol.com.br>
2020-04-23 20:04:37 -0300

    quase terminando

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,15 @@
 import "bootstrap";
+
+const initUpdateNavbarOnScroll = () => {
+  const navbar = document.getElementById('nav');
+  if (navbar) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= 1) {
+        navbar.classList.add('nav-scrolled');
+      } else
+        navbar.classList.remove('nav-scrolled');
+    });
+  }
+}
+
+initUpdateNavbarOnScroll();
